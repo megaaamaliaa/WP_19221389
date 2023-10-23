c33fd20c95ef876d2ca2140dfd993f997824cb18
megaaamaliaa <megaaamalia02@gmail.com>
2023-10-23 20:28:10 +0700

    menambah login

---
+++ application/views/templates/aute_footer.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+		<!-- Bootstrap core JavaScript-->
+	<script src="<?= base_url('assets/'); ?>vendor/jquery/jquery.min.js"></script>
+	<script src="<?= base_url('assets/'); ?>vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+	<!-- Core plugin JavaScript-->
+	<script src="<?= base_url('assets/'); ?>vendor/jquery-easing/jquery.easing.min.js"></script>
+	<!-- Custom scripts for all pages-->
+	<script src="<?= base_url('assets/'); ?>js/sb-admin-2.min.js"></script>
+	<script>
+	$('.alert-message').alert().delay(3000).slideUp('slow');
+	</script>
+</body>
+</html>
